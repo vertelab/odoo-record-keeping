5bb8b5af179fae3e6e168f1901414634636330d0
han-wong <han.wong@vertel.se>
2021-09-20 20:07:46 +0000

    fix: small bugs

--- record-keeping/models/ir_attachment.py
+++ record-keeping/models/ir_attachment.py
@@ -3,15 +3,25 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class IrAttachment(models.Model):
-    _inherit = "ir.attachment"
+    _inherit = 'ir.attachment'
 
     active = fields.Boolean(
-        string="Archived",
         default=True,
-        help="If an attachment is set to archived, it is not displayed.",
+        string='Archived',
+        help='Uncheck this to archive the document. It will not be displayed.',
     )
-
-    receiver = fields.Many2one('res.partner')
-    secrecy = fields.Boolean(default=False)
-    sender = fields.Many2one('res.partner')
+    is_secret = fields.Boolean(
+        default=False,
+        string='Is subject to secrecy',
+        help='Check this if document contains information that is subject to'
+             'secrecy. Combine with proper law section.'
+    )
+    is_official_document = fields.Boolean(
+        default=False,
+        string='Is official document',
+        help='Check this if the document is official.'
+    )
+    receiver_id = fields.Many2one('res.partner')
+    sender_id = fields.Many2one('res.partner')
