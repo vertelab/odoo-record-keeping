7f43879ea392f0f86d022f66eb3642e3b3afb34a
han-wong <han.wong@vertel.se>
2021-10-02 12:17:41 +0000

    update is_secret field logic

--- record-keeping_law_section/models/rk_law_section.py
+++ record-keeping_law_section/models/rk_law_section.py
@@ -9,11 +9,11 @@ _logger = logging.getLogger(__name__)
 class RecordKeepingLawSection(models.Model):
     """
     In order for a public authority to refuse to disclose information in an of-
-    ficial document to a natural or legal person, or any kind of information 
-    to another public authority, in principle the information must be subject 
-    to secrecy. The public authority must examine whether there is any secrecy 
-    provision that may cover the information in question. If there is not, the 
-    information is public in all circumstances and must be disclosed. 
+    ficial document to a natural or legal person, or any kind of information
+    to another public authority, in principle the information must be subject
+    to secrecy. The public authority must examine whether there is any secrecy
+    provision that may cover the information in question. If there is not, the
+    information is public in all circumstances and must be disclosed.
     """
     _name = 'rk.law.section'
     _description = 'Record keeping law section. '
@@ -28,12 +28,23 @@ class RecordKeepingLawSection(models.Model):
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
+    is_secret = fields.Boolean(compute='_compute_is_secret')
     law_section_ids = fields.Many2many('rk.law.section')
 
+    @api.depends('law_section_ids')
+    def _compute_is_secret(self):
+        for record in self:
+            record.is_secret = len(record.law_section_ids) > 0
+
+    @api.model
+    def create(self, vals):
+        res = super(IrAttachment, self).create(vals) 
+        _logger.warning(f'law section create-{self}')
+        return res
+
     def write(self, vals):
-        if vals.get('is_secret'):
-            if not vals.get('law_section_ids'):
-                raise ValidationError(_('Law Section is required when marking an official document as secret.'))
-        else:
-            vals.update({'law_section_ids': [(5, 0, 0)]})
-        return super(IrAttachment, self).write(vals)
+        
+        
+        res = super(IrAttachment, self).write(vals)
+        _logger.warning(f'HAN-----------------------{res}')
+        return res
