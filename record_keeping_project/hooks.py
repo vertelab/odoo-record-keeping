2125c373506e602a705dc7c14bc19c066518ddec
hanwong-vertel <han.wong@vertel.se>
2021-10-13 04:51:28 +0000

    record_keeping_project updated translation and other stuff

--- record_keeping_project/hooks.py
+++ record_keeping_project/hooks.py
@@ -10,7 +10,10 @@ def post_init_hook(cr, registry):
     This post-init-hook will create rk.document records for each existing project.
     """
     env = api.Environment(cr, SUPERUSER_ID, dict())
-    projects = env['project.project'].search([], order='id')
-    for project in projects:
-        if not project.rk_id:
-            project.rk_id = env['rk.document'].create({})
+    models = ['project.project', 'project.task']
+
+    for model in models:
+        records = env[model].search([], order='id')
+        for record in records:
+            if not record.rk_id:
+                record.rk_id = env['rk.document'].create({})
