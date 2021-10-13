895b65cdefe8f072d4038744b6481095b303260a
hanwong-vertel <han.wong@vertel.se>
2021-10-13 14:23:01 +0000

    record_keeping_project updated translation

--- record_keeping_project/models/project.py
+++ record_keeping_project/models/project.py
@@ -9,13 +9,12 @@ _logger = logging.getLogger(__name__)
 
 class ProjectProject(models.Model):
     _name = 'project.project'
-    _description = "Project"
     _inherit = ['mail.thread', 'project.project']
     _inherits = {'rk.document': 'rk_id'}
 
     rk_id = fields.Many2one(
         comodel_name='rk.document',
-        help='Link to corresponding record-keeping document',
+        help='The record-keeping document of this project',
         ondelete='restrict',
         required=True,
         readonly=True,
@@ -25,13 +24,12 @@ class ProjectProject(models.Model):
 
 class ProjectTask(models.Model):
     _name = 'project.task'
-    _description = "Extend Project Task with Record-keeping Mixin"
     _inherit = ['project.task']
     _inherits = {'rk.document': 'rk_id'}
 
     rk_id = fields.Many2one(
         comodel_name='rk.document',
-        help='Link to corresponding record-keeping document',
+        help='The record-keeping document of this task',
         ondelete='restrict',
         required=True,
         readonly=True,
