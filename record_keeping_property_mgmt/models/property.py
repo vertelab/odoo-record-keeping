d4535815abc1f8b73f21ba50a3e74dba1b59ba17
hanwong-vertel <han.wong@vertel.se>
2021-10-14 20:17:07 +0000

    record_keeping_mgmt added mail.thread functionality and translations

--- record_keeping_property_mgmt/models/property.py
+++ record_keeping_property_mgmt/models/property.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 class PropertyProperty(models.Model):
     _name = 'property.property'
-    _inherit = ['property.property']
+    _inherit = ['mail.thread', 'property.property']
     _inherits = {'rk.document': 'rk_id'}
 
     rk_id = fields.Many2one(
