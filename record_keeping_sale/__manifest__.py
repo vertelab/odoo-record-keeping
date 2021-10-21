d29ab77018e4b18dfb60382cac97c89005b1b326
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 15:10:51 +0200

    Update __manifest__.py

--- record_keeping_sale/__manifest__.py
+++ record_keeping_sale/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,21 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
 
 {
     'name': 'Record-keeping Sale',
     'summary': 'Record-keeping Sale for Odoo',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
+    'category': 'Administration',
+    'version': '14.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
     'description': """
 This module extends sale orders with record-keeping fields
 """,
-    'version': '14.0.1',
-    'category': 'Administration',
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se',
-    'author': 'Vertel AB',
     'depends': ['record_keeping', 'sale'],
     'data': [
         'views/sale_views.xml',
