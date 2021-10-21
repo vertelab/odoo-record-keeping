a7be58471308a04b4a1d20d2ccd74ee707891c7e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 14:45:33 +0200

    Update __manifest__.py

--- record_keeping/__manifest__.py
+++ record_keeping/__manifest__.py
@@ -22,14 +22,18 @@
 {
     'name': 'Record-keeping',
     'summary': 'Record-keeping for Odoo',
-    'description': """
-This module is the base module for Record-keeping
-""",
-    'version': '14.0.1',
-    'category': 'Administration',
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se',
     'author': 'Vertel AB',
+    'contributor': 'Mitchell Admin',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
+    'category': 'Administration',
+    'version': '14.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',   
+    'description': """
+    This module is the base module for Record-keeping
+    """,
     'depends': ['mail'],
     'data': [
         'data/rk.classification.csv',
