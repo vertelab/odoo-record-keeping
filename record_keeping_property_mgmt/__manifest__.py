c824d9fa96422b2cd40bb5b5ef5f9b9ddd58d6a7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 15:07:52 +0200

    Update __manifest__.py

--- record_keeping_property_mgmt/__manifest__.py
+++ record_keeping_property_mgmt/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,14 +22,18 @@
 {
     'name': 'Record-keeping Property Management',
     'summary': 'Record-keeping Property Management for Odoo',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keepin.git',
+    'category': 'Administration',
+    'version': '14.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
     'description': """
 This module extends properties with record-keeping fields
 """,
-    'version': '14.0.1',
-    'category': 'Administration',
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se',
-    'author': 'Vertel AB',
     'depends': ['property_mgmt', 'record_keeping'],
     'data': [
         'views/property_views.xml',
