5160dca6006a71713e40e7ba304bc4f60f71272b
hanwong-vertel <han.wong@vertel.se>
2021-11-29 15:07:46 +0000

    refactored record-keeping_property_mgmt

--- record_keeping_property_mgmt/__manifest__.py
+++ record_keeping_property_mgmt/__manifest__.py
@@ -27,12 +27,12 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-record-keepin.git',
     'category': 'Administration',
-    'version': '14.0.1',
+    'version': '14.0.2.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
     'website': 'https://vertel.se/record-keeping',
     'description': """
-This module extends properties with record-keeping fields
+This module extends Properties with record-keeping fields
 """,
     'depends': ['property_mgmt', 'record_keeping'],
     'data': [
--- record_keeping_property_mgmt/models/property.py
+++ record_keeping_property_mgmt/models/property.py
@@ -6,28 +6,28 @@ from odoo import _, api, fields, models
 class Property(models.Model):
     _name = 'property.property'
     _inherit = ['mail.thread', 'property.property']
-    _inherits = {'rk.document': 'rk_id'}
+    _inherits = {'rk.document': 'document_id'}
 
-    rk_id = fields.Many2one(
+    document_id = fields.Many2one(
         comodel_name='rk.document',
-        help='The record-keeping document of this property',
+        help='The record-keeping document id',
         ondelete='restrict',
-        required=True,
         readonly=True,
-        string='Record-keeping Document',
+        required=True,
+        string='Document',
     )
-
-    rk_ref = fields.Reference(
-        help='The record-keeping document of this property',
+    document_ref = fields.Reference(
+        compute='_compute_document_ref',
+        help='The record-keeping document reference',
         readonly=True,
         selection='_selection_target_model',
-        string='Registration Number',
+        string='Document Reference',
     )
 
-    @api.model
-    def _selection_target_model(self):
-        models = self.env['ir.model'].search([('model', '=', 'rk.document')])
-        return [(model.model, model.name) for model in models]
+    @api.depends('document_id')
+    def _compute_document_ref(self):
+        for rec in self:
+            rec.document_ref = f"rk.document,{rec.document_id.id or 0}"
 
     @api.onchange('is_official')
     def _onchange_is_official(self):
@@ -41,9 +41,13 @@ class Property(models.Model):
             self.secrecy_grounds = False
 
     @api.model
+    def _selection_target_model(self):
+        models = self.env['ir.model'].search([('model', '=', 'rk.document')])
+        return [(model.model, model.name) for model in models]
+
+    @api.model
     def create(self, vals):
-        property = super(Property, self).create(vals)
-        property.rk_ref = f'{property.rk_id._name},{property.rk_id.id}'
-        property.rk_res_model = property._name
-        property.rk_res_id = property.id
-        return property
+        record = super(Property, self).create(vals)
+        vals = {'res_model': record._name, 'res_id': record.id}
+        record.document_id = self.env['rk.document'].create(vals)
+        return record
