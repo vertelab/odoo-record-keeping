653a938a04f84d5b04ac884ab2eee585965d55ee
hanwong-vertel <han.wong@vertel.se>
2021-11-29 15:08:05 +0000

    refactored record-keeping_sale

--- record_keeping_sale/models/sale.py
+++ record_keeping_sale/models/sale.py
@@ -6,28 +6,28 @@ from odoo import _, api, fields, models
 class Order(models.Model):
     _name = 'sale.order'
     _inherit = ['sale.order']
-    _inherits = {'rk.document': 'rk_id'}
+    _inherits = {'rk.document': 'document_id'}
 
-    rk_id = fields.Many2one(
+    document_id = fields.Many2one(
         comodel_name='rk.document',
-        help='The record-keeping document of this sale order',
+        help='The record-keeping document id',
         ondelete='restrict',
-        required=True,
         readonly=True,
-        string='Record-keeping Document',
+        required=True,
+        string='Document',
     )
-
-    rk_ref = fields.Reference(
-        help='The record-keeping document of this sale order',
+    document_ref = fields.Reference(
+        compute='_compute_document_ref',
+        help='The record-keeping document reference',
         readonly=True,
         selection='_selection_target_model',
-        string='Registration Number',
+        string='Document Reference',
     )
 
-    @api.model
-    def _selection_target_model(self):
-        models = self.env['ir.model'].search([('model', '=', 'rk.document')])
-        return [(model.model, model.name) for model in models]
+    @api.depends('document_id')
+    def _compute_document_ref(self):
+        for rec in self:
+            rec.document_ref = f"rk.document,{rec.document_id.id or 0}"
 
     @api.onchange('is_official')
     def _onchange_is_official(self):
@@ -41,9 +41,13 @@ class Order(models.Model):
             self.secrecy_grounds = False
 
     @api.model
+    def _selection_target_model(self):
+        models = self.env['ir.model'].search([('model', '=', 'rk.document')])
+        return [(model.model, model.name) for model in models]
+
+    @api.model
     def create(self, vals):
-        order = super(Order, self).create(vals)
-        order.rk_ref = f'{order.rk_id._name},{order.rk_id.id}'
-        order.rk_res_model = order._name
-        order.rk_res_id = order.id
-        return order
+        record = super(Order, self).create(vals)
+        vals = {'res_model': record._name, 'res_id': record.id}
+        record.document_id = self.env['rk.document'].create(vals)
+        return record
