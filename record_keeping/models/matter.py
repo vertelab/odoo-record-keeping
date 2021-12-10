c9de7588275cc462726cf8d82e0631a1e598d7c1
Han Wong <han.wong@vertel.se>
2021-12-10 14:48:58 +0100

    added new field to rk.matter and changed access rights for user

--- record_keeping/models/matter.py
+++ record_keeping/models/matter.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _, api, fields, models
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class Matter(models.Model):
     _name = 'rk.matter'
@@ -54,11 +55,28 @@ class Matter(models.Model):
         string='Registration number',
         store=True,
     )
+    write_message = fields.Html(
+        compute='_compute_write_message',
+        string='Contents', 
+        )
 
     @api.depends('matter_name', 'reg_no')
     def _compute_name(self):
         for record in self:
             record.name = f'{record.reg_no or ""} {record.matter_name or ""}'
+
+    @api.depends('message_ids')
+    def _compute_write_message(self):
+        for record in self:
+            if record.message_ids:
+                description = record.message_ids[0].description
+                tracking_values = record.message_ids[0].tracking_value_ids
+                if description:
+                    record.write_message = description
+                elif tracking_values:
+                    record.write_message = (f"{tracking_values[0].field_desc} -> {tracking_values[0].get_new_display_value()[0]}")
+            else:
+                record.write_message = ''
 
     @api.model
     def create(self, vals):
