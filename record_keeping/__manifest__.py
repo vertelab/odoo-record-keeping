f068351c63fc4851be5e91aba8031e85262788b7
hanwong-vertel <han.wong@vertel.se>
2021-12-13 10:34:58 +0000

    updated translations

--- record_keeping/__manifest__.py
+++ record_keeping/__manifest__.py
@@ -27,7 +27,7 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
     'category': 'Administration',
-    'version': '14.0.2.0.2',
+    'version': '14.0.2.0.3',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
     'website': 'https://vertel.se/record-keeping',   
