6f00e1ed0a15ee40c070f1c3fcd50854df1f6baa
Han Wong <han.wong@vertel.se>
2022-02-09 14:29:40 +0000

    fix: moved to record_keeping

--- record_keeping_sale/models/sale.py
+++ record_keeping_sale/models/sale.py
@@ -13,28 +13,28 @@ class SaleOrder(models.Model):
     _inherits = {'rk.document': 'document_id'}
 
     
-    def message_post(self, **kwargs):
-        self = self.sudo()
-        _logger.warning(f"{self._description=}")
-        res = super().message_post(**kwargs)
-        matter_id = self.matter_id.id or False
-        fields = self.env['rk.mail'].fields_get()
-        _logger.warning(f"{res=}")
-        _logger.warning(f"{res.mail_ids=}")
-        for mail in res.mail_ids:
-            vals = {'name': mail['subject']}
-            for key in fields.keys():
-                if hasattr(mail, key):
-                    if fields[key]['type'] in ['many2many']:
-                        vals[key] = mail[key].ids
-                    elif fields[key]['type'] in ['many2one']:
-                        vals[key] = mail[key].id
-                    else:
-                        vals[key] = mail[key]
-            if matter_id:
-                vals['matter_id'] = matter_id
-                vals['is_official'] = True
-            _logger.warning(f"{vals=}")
-            mail = self.env['rk.mail'].create(vals)
-            # mail.document_id.matter_id = matter_id
-        return res
+    # def message_post(self, **kwargs):
+    #     self = self.sudo()
+    #     _logger.warning(f"{self._description=}")
+    #     res = super().message_post(**kwargs)
+    #     matter_id = self.matter_id.id or False
+    #     fields = self.env['rk.mail'].fields_get()
+    #     _logger.warning(f"{res=}")
+    #     _logger.warning(f"{res.mail_ids=}")
+    #     for mail in res.mail_ids:
+    #         vals = {'name': mail['subject']}
+    #         for key in fields.keys():
+    #             if hasattr(mail, key):
+    #                 if fields[key]['type'] in ['many2many']:
+    #                     vals[key] = mail[key].ids
+    #                 elif fields[key]['type'] in ['many2one']:
+    #                     vals[key] = mail[key].id
+    #                 else:
+    #                     vals[key] = mail[key]
+    #         if matter_id:
+    #             vals['matter_id'] = matter_id
+    #             vals['is_official'] = True
+    #         _logger.warning(f"{vals=}")
+    #         mail = self.env['rk.mail'].create(vals)
+    #         # mail.document_id.matter_id = matter_id
+    #     return res
