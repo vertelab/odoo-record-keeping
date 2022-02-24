6c6cda37abaab162ebcb98d0bef97f9a410dcef8
Han Wong <han.wong@vertel.se>
2022-02-24 14:35:31 +0000

    [IMP] add new field data if exists on sale.order

--- record_keeping_sale_project/models/task.py
+++ record_keeping_sale_project/models/task.py
@@ -6,6 +6,7 @@ from odoo.exceptions import UserError
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
@@ -16,13 +17,17 @@ class ProjectTask(models.Model):
         else:
             self.create_matter()
             if not self.sale_order_id:
-                self.sale_order_id = self.env['sale.order'].create(dict(
-                    matter_id=self.matter_id.id,
-                    is_official=True,
-                    partner_id=self.partner_id.id,
-                    project_id=False,
-                ))
-            
+                SaleOrder= self.env['sale.order']
+                vals = {
+                    'matter_id': self.matter_id.id,
+                    'is_official': True,
+                    'partner_id': self.partner_id.id,
+                    'project_id': False,
+                }
+                if SaleOrder.fields_get().get('name_description'):
+                    vals['name_description'] = self.name
+                self.sale_order_id = self.env['sale.order'].create(vals)
+
     def create_matter(self):
         self.ensure_one()
         if self.sale_order_id and self.sale_order_id.matter_id:
@@ -31,4 +36,3 @@ class ProjectTask(models.Model):
             super().create_matter()
             if self.sale_order_id and not self.sale_order_id.matter_id:
                 self.sale_order_id.matter_id = self.matter_id
-        
