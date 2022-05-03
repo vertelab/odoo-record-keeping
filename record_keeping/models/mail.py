504b4d6b2195575cf9719d4f67ebcc8425f0513f
Han Wong <han.wong@vertel.se>
2022-05-03 11:51:10 +0000

    [FIX] expose draw_up_receive_date and write to sender and receiver fields

--- record_keeping/models/mail.py
+++ record_keeping/models/mail.py
@@ -121,6 +121,11 @@ class Mail(models.Model):
                         values[key] = mail[key].id
                     else:
                         values[key] = mail[key]
+            values['sender'] = mail.email_from
+            receivers = [mail.email_to] if mail.email_to else []
+            recipients = [recipient_id.email_formatted for recipient_id in mail.recipient_ids]
+            values['receiver'] = ', '.join(receivers + recipients)
+
             if (model := mail.model) and (res_id := mail.res_id):
                 if (rec := self.env[model].browse(res_id)):
                     if hasattr(rec, 'matter_id'):
