cbd68b34bfae044ac703d1dcdc43419fddfc67d2
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-19 11:48:32 +0200

    Update __manifest__.py
    Fixing with details to the manifest.

--- record_keeping_project/__manifest__.py
+++ record_keeping_project/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-################################################################################
+##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,28 +15,30 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+##############################################################################
 
 {
-    'name': 'Record-keeping Project',
-    'summary': 'Record-keeping Project for Odoo',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
-    'category': 'Administration',
+    'name': 'Record-keeping: Project',
     'version': '14.0.2.2.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/record-keeping',
+    'summary': 'Record-keeping Project for Odoo',
+    'category': 'Administration',
     'description': """
 This module extends projects with record-keeping fields\n
 v14.1.1.0 added new version number.   \n
 v14.0.2.1.0 added wizard to link to record
 v14.0.2.2.0 refactored classes
 """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/record-keeping/record-keeping-project',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
     'depends': ['mail', 'project', 'record_keeping', 'record_keeping_wizard'],
     'data': [
         'views/project_views.xml',
