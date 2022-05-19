5837747423f46892395facef953ff16154edea36
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-19 12:01:56 +0200

    Update __manifest__.py
    Fixing details in the manifest-file.

--- record_keeping_wizard/__manifest__.py
+++ record_keeping_wizard/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-################################################################################
+##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
 #    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
@@ -17,23 +17,25 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+##############################################################################
 
 {
-    'name': 'Record-keeping Wizard',
-    'summary': 'Record-keeping Wizard for Odoo',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
-    'category': 'Administration',
+    'name': 'Record-keeping: Wizard',
     'version': '14.0.2.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/record-keeping',
+    'summary': 'Record-keeping  Wizard for Odoo',
+    'category': 'Administration',
     'description': """
 This module extends multiple modules with a button and a wizard to link actions to matter
 """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/record-keeping/record-keeping-wizard',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
     'depends': ['record_keeping'],
     'data': [
         'views/matter_wizard.xml',
