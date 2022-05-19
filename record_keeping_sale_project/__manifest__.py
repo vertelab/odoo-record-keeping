e6937704be099b2693f4abce5ea726d1ef519e3f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-19 11:59:40 +0200

    Update __manifest__.py
    Fixing details in manifest.

--- record_keeping_sale_project/__manifest__.py
+++ record_keeping_sale_project/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-################################################################################
+##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,26 +15,28 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+##############################################################################
 
 {
-    'name': 'Record-keeping Sale Project',
-    'summary': 'Record-keeping Sale Project for Odoo',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
-    'category': 'Administration',
-    'version': '14.0.0.1.0',
+    'name': 'Record-keeping: Sale Project',
+    'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/record-keeping',
+    'summary': 'Record-keeping  Sale Project for Odoo',
+    'category': 'Administration',
     'description': """
 This module extends projects and sales with record-keeping buttons\n
 v14.0.0.0.1 Stage Quote Sent added
 """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/record-keeping/record-keeping-sale-project',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
     'depends': ['record_keeping_sale', 'record_keeping_project', 'sale_project'],
     'data': [
         'data/project_task_type.xml',
