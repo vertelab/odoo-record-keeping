31dcbd2cb86c6476339383da628b65c3bee403f9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-19 11:45:13 +0200

    Update __manifest__.py
    Fixing details with the manifest file.

--- record_keeping_hr/__manifest__.py
+++ record_keeping_hr/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-################################################################################
+##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,25 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+##############################################################################
 
 {
-    'name': 'Record-keeping HR',
-    'summary': 'Record-keeping HR for Odoo',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
-    'category': 'Administration',
+    'name': 'Record-keeping: HR',
     'version': '14.0.2.0.3',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/record-keeping',
+    'summary': 'Record-keeping HR for Odoo',
+    'category': 'Administration',
     'description': """
 This module extends Employees with record-keeping fields
 """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/record-keeping/record-keeping-hr',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping.git',
     'depends': ['hr', 'record_keeping'],
     'data': [
         'views/hr_views.xml',
