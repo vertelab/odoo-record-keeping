853b300623d99689a2a7e6ff9125fe9a96baee1b
Han Wong <han.wong@vertel.se>
2022-06-23 14:54:41 +0200

    [IMP] link tasks to same matter if created by sale.order

--- record_keeping_project/models/task.py
+++ record_keeping_project/models/task.py
@@ -5,3 +5,13 @@ from odoo import _, api, fields, models
 class Task(models.Model):
     _name = 'project.task'
     _inherit = ['project.task', 'rk.document.mixin']
+
+    @api.model
+    def create(self, vals):
+        if not 'matter_id' in vals and (sale_order_id := vals.get('sale_order_id')):
+            if (matter_id := self.env['sale.order'].browse(sale_order_id).matter_id):
+                vals['matter_id'] = matter_id.id
+        return super().create(vals)
+        
+
+
