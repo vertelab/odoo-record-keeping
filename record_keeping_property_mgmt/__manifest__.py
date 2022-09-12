b7f28d3d98086cfc51482c8c8952c80e79eb079a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 11:16:25 +0200

    Update __manifest__.py

--- record_keeping_property_mgmt/__manifest__.py
+++ record_keeping_property_mgmt/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.2.0.3',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Record-keeping Property Management for Odoo',
-    'category': 'Administration',
+    'category': 'Government',
     'description': """
 This module extends Properties with record-keeping fields
 """,
