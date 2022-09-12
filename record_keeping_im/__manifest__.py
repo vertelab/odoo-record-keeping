ce295b0500db1b369c100d9034566bfc4d7f3ab1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 11:14:37 +0200

    Update __manifest__.py

--- record_keeping_im/__manifest__.py
+++ record_keeping_im/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Record-keeping for Odoo IM Live Chat',
-    'category': 'Administration',
+    'category': 'Government',
     #'sequence': '1'
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/record-keeping/record_keeping_im',
