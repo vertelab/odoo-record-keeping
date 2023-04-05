3f1b5f1de108f67208ea4773e299b0697b36a2c4
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-04-05 09:13:03 +0200

    Update __manifest__.py
    minor spelling error

--- record_keeping/__manifest__.py
+++ record_keeping/__manifest__.py
@@ -33,7 +33,7 @@
     """,
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/record-keeping/record_keeping',
+    'website': 'https://vertel.se/apps/odoo-record-keeping/record_keeping',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
