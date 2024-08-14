be0a8dba45d7cab32e2d3e1d28ff80b955229187
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-14 14:00:10 +0200

    Update __manifest__.py

--- record_keeping_attachment/__manifest__.py
+++ record_keeping_attachment/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,8 +21,8 @@
 
 {
     'name': 'Record-keeping: Attachment',
-    'version': '14.0.2.1.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '2.1.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Record-keeping Attachment for Odoo',
     'category': 'Government',
     'description': """
