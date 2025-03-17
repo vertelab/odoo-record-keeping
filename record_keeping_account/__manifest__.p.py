d7740e9589e261dca9c08d618241fc032c2ed50b
marcuszxc <marcus.karlsson@vertel.se>
2025-03-17 15:46:02 +0000

    record_keeping_account, record_keeping_event, record_keeping_project, record_keeping_wizard: p-filed

---
+++ record_keeping_account/__manifest__.p.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Record-keeping: Account',
+    'version': '2.1.2',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Record-keeping for Odoo Account',
+    'category': 'Government',
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-record-keeping/record_keeping_account',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-record-keeping',
+    'description': """
+        This module extends Account with record-keeping fields
+    """,
+    'depends': ['account', 'record_keeping', 'sale', 'record_keeping_wizard'],
+    'data': [
+        'views/account_views.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
---
+++ record_keeping_event/models/__init__.p.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import event
+# from . import matter_wizard
