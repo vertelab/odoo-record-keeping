8fc89f532fa02d8e859aed6fa2b3fd56e9ac2046
marcuszxc <marcus.karlsson@vertel.se>
2025-03-17 10:30:38 +0000

    record_keeping: Fully p-filed

---
+++ record_keeping/models/document_mixin.p.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class DocumentMixin(models.AbstractModel):
+    _name = 'rk.document.mixin'
+    _description = 'Document Mixin'
+    _inherits = {'rk.document': 'document_id'}
+
+    document_id = fields.Many2one(
+        auto_join=True,
+        comodel_name='rk.document',
+        copy=False,
+        help='The record-keeping document id',
+        ondelete='restrict',
+        required=True,
+        string='Document',
+        index=True
+    )
+    document_ref = fields.Reference(
+        compute='_compute_document_ref',
+        copy=False,
+        help='The record-keeping document reference',
+        selection='_selection_target_model',
+        string='Document Reference',
+    )
+
+    @api.depends('document_id')
+    def _compute_document_ref(self):
+        for record in self:
+            record.document_ref = f"rk.document,{record.document_id.id or 0}"
+            record._get_document_link()
+
+    def _get_default_param(self, field):
+        param = f"record_keeping.{self._name.replace('.', '_')}_default_{field}"
+        if res := self.env['ir.config_parameter'].sudo().get_param(param):
+            res = int(res)
+        return res
+
+    def _get_document_link(self):
+        _logger.warning("_get_document_link"*100)
+        self.ensure_one()
+        vals = dict(res_model=self._name, res_id=self.id)
+        _logger.warning(f"{vals=}")
+        if document := self.document_id:
+            _logger.warning(f"{document=} {document.res_model=} {self._name=} {document.res_id=} {self.id=}")
+            if document.res_model != self._name or document.res_id != self.id:
+                document.write(vals)
+        else:
+            return vals
+
+    @api.model
+    def _selection_target_model(self):
+        models = self.env['ir.model'].search([('model', '=', 'rk.document')])
+        return [(model.model, model.name) for model in models]
+
+    @api.model
+    def create(self, vals):
+        for field in ['classification_id', 'document_type_id']:
+            if not field in vals:
+                vals[field] = self._get_default_param(field)
+        record = super().create(vals)
+        record._get_document_link()
+        return record
+
+    def create_matter(self):
+        self.ensure_one()
+        if not self.matter_id:
+            self.is_official = True
+            self.matter_id = self.env['rk.matter'].create({})
+
+    def write(self, vals):
+        for record in self:
+            if document_vals := record._get_document_link():
+                vals['document_id'] = self.env['rk.document'].create(document_vals)
+        result = super().write(vals)
+        return result
---
+++ record_keeping/models/mixin.p.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+
+
+class Mixin(models.AbstractModel):
+    _name = 'rk.mixin'
+    _description = 'Mixin'
+
+    active = fields.Boolean(
+        copy=False,
+        default=True,
+        string='Archived',
+        tracking=True,
+    )
+    document_type_id = fields.Many2one(
+        comodel_name='rk.document.type',
+        copy=False,
+        string='Document Type',
+        tracking=True,
+    )
+    draw_up_date = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today(),
+        help='The date when the document is ready to be used or sent',
+        index=True,
+        string='Drawn up',
+        tracking=True,
+    )
+    draw_up_receive_date = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today(),
+        help='The date when the document is ready to be used or sent',
+        index=True,
+        string='Drawn up/Received',
+        tracking=True,
+    )
+    is_official = fields.Boolean(
+        copy=False,
+        default=False,
+        help='Check this option if this document is an official document',
+        string='Official document',
+        tracking=True,
+    )
+    is_secret = fields.Boolean(
+        copy=False,
+        default=False,
+        help='Check this option if it can be assumed that information contained '
+             'in this document should not be disclosed on grounds of secrecy.',
+        string='Secrecy marker',
+        tracking=True,
+    )
+    law_section_id = fields.Many2one(
+        comodel_name='rk.law.section',
+        copy=False,
+        help='The specified secrecy provision when the document has a secrecy marker',
+        string='Secrecy provision',
+        tracking=True,
+    )
+    receive_date = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today(),
+        help='The date when the document has been received by a competent person.',
+        index=True,
+        string='Received',
+        tracking=True,
+    )
+    receiver = fields.Char(
+        copy=False,
+        help='The competent person who received the document',
+        string='Receiver ',
+        tracking=True,
+    )
+    receiver_id = fields.Many2one(
+        comodel_name='res.partner',
+        copy=False,
+        help='The competent person who received the document',
+        string='Receiver',
+        tracking=True,
+    )
+    secrecy_grounds = fields.Char(
+        copy=False,
+        default=False,
+        help='If marked as secret, please provide more information',
+        string='Secrecy grounds',
+        tracking=True,
+    )
+    sender = fields.Char(
+        copy=False,
+        help='The person who sent this document',
+        string='Sender ',
+        tracking=True,
+    )
+    sender_id = fields.Many2one(
+        comodel_name='res.partner',
+        copy=False,
+        help='The person who sent this document',
+        string='Sender',
+        tracking=True,
+    )
+
+    _sql_constraints = [
+        ('is_secret_requires_provision',
+         "CHECK(is_secret IS NOT TRUE OR law_section_id IS NOT NULL)",
+         #  "CHECK(is_secret IS NOT TRUE OR (law_section_id IS NOT NULL AND secrecy_grounds IS NOT NULL))",
+         'Please provide legal provision')]
+
+    def _get_default_classification(self):
+        param = 'record_keeping.matter_default_classification_id'
+        return int(self.env['ir.config_parameter'].sudo().get_param(param))
+
+    @api.onchange('is_official')
+    def _onchange_is_official(self):
+        if not self.is_official:
+            self.is_secret = False
+
+    @api.onchange('is_secret')
+    def _onchange_is_secret(self):
+        if not self.is_secret:
+            self.law_section_id = False
+            self.secrecy_grounds = False
