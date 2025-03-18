0d4d418326a5794ceafbae2172dacfe432e1fed6
vertelbot <vertelbot@vertel.se>
2025-03-18 09:56:55 +0000

    odoobranchpfile odoo-record-keeping from 18.0. Do not p-file sync

--- record_keeping_mail_channel/models/mail_channel.py
+++ record_keeping_mail_channel/models/mail_channel.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 from odoo import _, api, fields, models
-
 
 class MailChannel(models.Model):
     _name = 'mail.channel'
--- record_keeping_mail_channel/models/matter_wizard.p.py
+++ record_keeping_mail_channel/models/matter_wizard.p.py
@@ -9,10 +9,18 @@ class EventMatterWizard(models.TransientModel):
     _inherit = ['rk.wizard']
 
     def _get_model(self):
+        # #if VERSION <= "16.0"
         return self.env['mail.channel'].browse(self.env.context.get('active_ids'))
+        # #elif VERSION >= "17.0"
+        return self.env['discuss.channel'].browse(self.env.context.get('active_ids'))
+        # #endif
 
     model = fields.Many2one(
+        # #if VERSION <= "16.0"
         comodel_name='mail.channel',
+        # #elif VERSION >= "17.0"
+        comodel_name='discuss.channel',
+        # #endif
         default=_get_model,
         readonly=True,
     )
