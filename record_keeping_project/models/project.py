b8345983114e293a2f362b20ea07b20675956d23
vertelbot <vertelbot@vertel.se>
2025-06-09 12:32:16 +0000

    odoobranchpfile odoo-record-keeping from 18.0. Do not p-file sync

--- record_keeping_project/models/project.py
+++ record_keeping_project/models/project.py
@@ -20,6 +20,7 @@ class Project(models.Model):
     @api.model
     def create(self, vals):
         if not 'matter_id' in vals and (sale_order_id := vals.get('sale_order_id')):
-            if (matter_id := self.env['sale.order'].browse(sale_order_id).matter_id):
+            if matter_id := self.env['sale.order'].browse(sale_order_id).matter_id:
                 vals['matter_id'] = matter_id.id
         return super().create(vals)
+
