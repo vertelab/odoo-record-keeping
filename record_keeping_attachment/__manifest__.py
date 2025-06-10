55380caec18830f75d9e910b34d22da0b36f62eb
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-06-10 09:24:20 +0000

    fixes so that we can install modules

--- record_keeping_attachment/__manifest__.py
+++ record_keeping_attachment/__manifest__.py
@@ -40,7 +40,7 @@
         'security/ir.model.access.csv',
         'wizard/add_file_wizard.xml',
         # 'views/ir_attachment_views.xml',
-        'views/matter_views.xml',
+        #'views/matter_views.xml',
     ],
 }
 # vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
