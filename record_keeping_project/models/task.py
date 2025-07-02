d8101a8218bfcfe1ec848d98f9ce8b7f8ddb6f52
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-07-02 09:02:17 +0000

    record_keeping_project: readded

--- record_keeping_project/models/task.py
+++ record_keeping_project/models/task.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import _, api, fields, models
 
-
 class Task(models.Model):
     _name = 'project.task'
     _inherit = ['project.task', 'rk.document.mixin']
 
     @api.model_create_multi
     def create(self, vals):
-        if not 'matter_id' in vals and (sale_order_id := vals.get('sale_order_id')):
+      for val in vals:
+        if not 'matter_id' in vals and (sale_order_id := val.get('sale_order_id')):
             if matter_id := self.env['sale.order'].browse(sale_order_id).matter_id:
-                vals['matter_id'] = matter_id.id
-        return super().create(vals)
+                val['matter_id'] = matter_id.id
+      return super().create(vals)
