f914ca0982212ca1993179a05d16e65cbb1eaeb9
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-07-02 07:27:33 +0000

    record_keeping: use name on the record as part of the matter

--- record_keeping_event/models/event.py
+++ record_keeping_event/models/event.py
@@ -5,3 +5,10 @@ from odoo import _, api, fields, models
 class Event(models.Model):
     _name = 'event.event'
     _inherit = ['event.event', 'rk.document.mixin']
+
+
+    def create_matter(self):
+        self.ensure_one()
+        super().create_matter()
+        self.matter_id.matter_name = f"{self.event_type_id.name} i {self.address_id.name} med startdatum {self.date_begin.date()}"
+
