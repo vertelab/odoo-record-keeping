3d277d0bfa97c11e143f523f193a25745b5fa227
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-08-12 16:21:29 +0200

    T/5100 record_keeping: readded so that a category is set. record_keeping: changed they view so it looks better in odoo versions where you are always in edit mode.

--- record_keeping/models/matter.py
+++ record_keeping/models/matter.py
@@ -18,6 +18,7 @@ class Matter(models.Model):
         comodel_name='rk.classification',
         copy=False,
         string='Classification',
+        default=lambda self: int(self._get_default_param('classification_id')) or 0,
         tracking=True,
         index=True
     )
