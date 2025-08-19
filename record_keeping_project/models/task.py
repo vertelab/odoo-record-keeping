5780b879e4381ea613392429df2d71daff9dd4c2
vertelbot <vertelbot@vertel.se>
2025-08-19 06:26:24 +0000

    odoobranchpfile odoo-record-keeping from 14.0. Do not p-file sync

--- record_keeping_project/models/task.py
+++ record_keeping_project/models/task.py
@@ -4,6 +4,7 @@ class Task(models.Model):
     _name = 'project.task'
     _inherit = ['project.task', 'rk.document.mixin']
     
+
     @api.model_create_multi
     def create(self, vals):
       connect_task_to_matter = self.env['ir.config_parameter'].sudo().get_param('record_keeping.project_task_default_automatic_matter_connection')
