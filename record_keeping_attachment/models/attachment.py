19d7248de9a7a1c348d4b251a7b0f52db8e4cc77
vertelbot <vertelbot@vertel.se>
2026-01-29 14:03:39 +0000

    odoobranchpfile odoo-record-keeping from 18.0. Do not p-file sync

--- record_keeping_attachment/models/attachment.py
+++ record_keeping_attachment/models/attachment.py
@@ -49,7 +49,8 @@ class Attachment(models.Model):
             return super().write(vals)
 
     def unlink(self):
-        if not self.env.user.has_group('record_keeping.group_rk_manager') and self.document_id.matter_id:
-            raise UserError(_("You are not authorized to delete a document linked to a matter"))
+        for record in self:
+            if not self.env.user.has_group('record_keeping.group_rk_manager') and record.document_id.matter_id:
+                raise UserError(_("You are not authorized to delete a document linked to a matter"))
         return super(Attachment, self).unlink()
 
